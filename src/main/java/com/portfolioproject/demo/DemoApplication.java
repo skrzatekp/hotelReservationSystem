5b42104919855214eeabc548ecdf87e29aa99964
skrzatekp <85845022+skrzatekp@users.noreply.github.com>
2021-06-14 17:21:58 +0200

    heroku 19

--- src/main/java/com/portfolioproject/demo/DemoApplication.java
+++ src/main/java/com/portfolioproject/demo/DemoApplication.java
@@ -2,10 +2,18 @@ package com.portfolioproject.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Controller
 @SpringBootApplication
 public class DemoApplication {
+
+    @RequestMapping()
+    String home() {
+        return "index";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
